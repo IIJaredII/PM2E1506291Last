4f8db019f623b0d2e2570722b9b48b0257e487c9
Stacy Sauceda <stacysauceda1200@gmail.com>
2025-02-15 23:15:11 -0600

    Modificacion de Principal 2

--- app/src/main/java/com/example/pm2e1506291/Acciones.java
+++ app/src/main/java/com/example/pm2e1506291/Acciones.java
@@ -1,28 +1,41 @@
 package com.example.pm2e1506291;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.pm2e1506291.Funciones.imageUtils;
 import com.example.pm2e1506291.Models.PaisesModel;
 import com.example.pm2e1506291.Repository.PaisesRepository;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Acciones extends AppCompatActivity {
     private int idpais;
+    private ImageView imagen;
+    private String imagenBit;
     private Button camara,galeria, compartir, actualizar, eliminar, llamar;
     private EditText nombre, numero, nota;
 
@@ -41,6 +54,44 @@ public class Acciones extends AppCompatActivity {
         nombre=(EditText) findViewById(R.id.editTextText3);
         numero=(EditText) findViewById(R.id.editTextPhone2);
         nota=(EditText) findViewById(R.id.editTextTextMultiLine2);
+
+        camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirCamara();
+            }
+        });
+
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AbrirGaleria();
+            }
+        });
+
+        llamar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String numero = "98360340";
+                if (ActivityCompat.checkSelfPermission(Acciones.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(Acciones.this, new String[]{android.Manifest.permission.CALL_PHONE}, 1);
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:" + numero));
+                    startActivity(intent);
+                }
+            }
+        });
+
+        compartir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT,"nombre: "+"\nNumero:");
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent, "Compartir con "));
+            }
+        });
 
 
         PaisesRepository paisesRepository = new PaisesRepository(this);
@@ -78,5 +129,54 @@ public class Acciones extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void AbrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+
+        galeriaARL.launch(intent);
+    }
+
+    private void abrirCamara() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    private ActivityResultLauncher<Intent> galeriaARL = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Uri uri = result.getData().getData(); // Obtener la URI de la imagen seleccionada
+                    if (uri != null) {
+                        try {
+                            // Convertir la URI en un Bitmap
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                            imagen.setImageBitmap(bitmap);
+                            imagenBit = imageUtils.encodeToBase64(bitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            Toast.makeText(this, R.string.imagenerror, Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(this, R.string.imagenerror, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, R.string.imagenusuario, Toast.LENGTH_SHORT).show();
+                }
+            }
+    );
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            imagen.setImageBitmap(bitmap);
+            imagenBit = imageUtils.encodeToBase64(bitmap);
+        }
     }
 }
