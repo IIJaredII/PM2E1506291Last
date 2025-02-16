026f5f75160fe34d4d9a5acd48bd088f45b6bf0c
IIJaredII <jaredrios2425@gmail.com>
2025-02-15 21:59:27 -0600

    Añadir items en spinner

--- app/src/main/java/com/example/pm2e1506291/DAO/PaisesDao.java
+++ app/src/main/java/com/example/pm2e1506291/DAO/PaisesDao.java
@@ -1,5 +1,8 @@
 package com.example.pm2e1506291.DAO;
 
+import com.example.pm2e1506291.configuracion.PaisesContract;
+
 public class PaisesDao {
-    public static String ObtenerCodigoPorId = "SELECT codigo FROM PAISES WHERE id = ?";
+    public static String ObtenerCodigoPorId = "SELECT codigo FROM "+PaisesContract.TABLE_NAME+" WHERE id = ?";
+    public static String ObtenerPaises = "SELECT * FROM "+ PaisesContract.TABLE_NAME;
 }
--- app/src/main/java/com/example/pm2e1506291/Principal.java
+++ app/src/main/java/com/example/pm2e1506291/Principal.java
@@ -5,7 +5,12 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -22,8 +27,11 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pm2e1506291.Funciones.imageUtils;
+import com.example.pm2e1506291.Models.PaisesModel;
+import com.example.pm2e1506291.Repository.PaisesRepository;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Principal extends AppCompatActivity {
 
@@ -62,6 +70,35 @@ public class Principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_principal);
+        PaisesRepository paisesRepository = new PaisesRepository(this);
+        final int[] idPais = {-1}; // Inicializar el ID del país seleccionado
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayList<PaisesModel> paises = paisesRepository.ObtenerPaises();
+
+        ArrayList<String> nombresPaises = new ArrayList<>();
+
+        for (PaisesModel pais : paises) {
+            nombresPaises.add(pais.getNombre());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nombresPaises);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                idPais[0] = paises.get(position).getId();
+                Toast.makeText(getApplicationContext(), "País seleccionado: " + paises.get(position).getNombre() + ", ID: " + idPais[0], Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No hacer nada
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
